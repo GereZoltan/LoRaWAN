460ae1df91322611fb591b6782607079fb7f5477
Zoltan Karoly Gere <zoltan.k.gere@gmail.com>
2021-11-27 11:16:58 +0200

    Activation parameters are separated into a config file

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import utime
+import machine
 # from utime import sleep_ms, sleep_us, ticks_ms, ticks_us, ticks_diff
 import SCC
 import LoRaWANHandler
+from LoRaConfig import LoRaConfig
+
+def blink(count, delay):
+    for ind in range(count):
+        led.on()
+        utime.sleep_ms(delay // 2)
+        led.off()
+        utime.sleep_ms(delay // 2)
 
 print("This is the LoRa temperature and humidity measurement application.")
 LoRaWANHandler.getBoardID()
 
-lh = LoRaWANHandler.LoRaWANHandler()
+LED_PIN = const(25)
+led = machine.Pin(LED_PIN, machine.Pin.OUT)
+
+lh = LoRaWANHandler.LoRaWANHandler(LoRaConfig)
+blink(3, 1000)
 lh.otaa()
+blink(3, 1000)
 i2c = SCC.initSCC()
 utime.sleep_ms(5000)
 
@@ -16,5 +30,6 @@ while(True):
     msg = str(meas[0]) + " " + str(meas[1])
     print(msg)
     lh.send(msg, False)
-    utime.sleep_ms(60000)
+    blink(2, 2000)
+    utime.sleep_ms(30000)
 
